f6123138ad25e2325bb71a141665477bf8e0c9db
Anna Vasileva <miss.niakris@gmail.com>
2023-11-04 23:19:09 +0300

    made it with loop

--- loops1.sh
+++ loops1.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#the string
-str=${1~~}
+#we can  do this task with rev and substitution, but the task is about loops so
+#str=${1~~}
+#echo $str | rev
 
+str=$1
+result=''
 
-echo $str | rev
+for (( i=${#str}-1; $i>=0; i-- )); do
+	result+=${str:$i:1}
+done
+
+echo ${result~~}
